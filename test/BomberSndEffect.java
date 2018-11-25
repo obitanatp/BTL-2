f2ffccd09ae572be88db6f675b73b0ccf7ba9b55
HoangTuanAnh <43361202+obitanatp@users.noreply.github.com>
2018-11-25 10:16:59 -0800

    Add files via upload

---
+++ test/BomberSndEffect.java
@@ -0,0 +1,29 @@
+package Java;
+import java.io.*;
+/**
+ * This class plays the background music.
+ */
+public class BomberSndEffect extends Thread {
+    public BomberSndEffect() {
+        start();
+    }
+
+    public void playSound(String str) {
+        /** if Java 2 is available */
+        if (Main.J2) {
+            SoundPlayer sound = null;
+            try {
+                /** create sound player */
+                sound = new SoundPlayer(
+                new File(BomberMain.RP +
+                "Sounds/BomberSndEffects/" +  str + ".mid").
+                getCanonicalPath());
+            }
+            catch (Exception e) { }
+            /** open file */
+            ((SoundPlayer)sound).open();
+            /** then play sound */
+            sound.change(0, false);
+        }
+    }
+}
